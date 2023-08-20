bfdfdf8e94f7317d17d13b74c4eba004b75e038f
kacper <106306274+kayYZ1@users.noreply.github.com>
2023-08-20 18:46:28 +0200

    new form

--- client/src/pages/login/LoginForm.tsx
+++ client/src/pages/login/LoginForm.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Button, Checkbox, Form, Input, Divider, Avatar } from 'antd';
-import { UserOutlined } from "@ant-design/icons"
+import { Button, Checkbox, Form, Input } from 'antd';
 
 const onFinish = (values: any) => {
   console.log('Success:', values);
@@ -11,35 +9,32 @@ const onFinishFailed = (errorInfo: any) => {
   console.log('Failed:', errorInfo);
 };
 
+type FieldType = {
+  username?: string;
+  password?: string;
+  remember?: string;
+};
+
 const LoginForm: React.FC = () => (
   <Form
     name="basic"
     labelCol={{ span: 8 }}
     wrapperCol={{ span: 16 }}
-    style={{ maxWidth: 600, paddingTop: 200, marginLeft: 100 }}
+    style={{ maxWidth: 600 }}
     initialValues={{ remember: true }}
     onFinish={onFinish}
     onFinishFailed={onFinishFailed}
     autoComplete="off"
   >
-    <Form.Item
-      label="E-mail"
-      name="email"
-      rules={[
-        {
-          type: 'email',
-          message: 'The input is not valid E-mail!',
-        },
-        {
-          required: true,
-          message: 'Please input your E-mail!',
-        },
-      ]}
+    <Form.Item<FieldType>
+      label="Username"
+      name="username"
+      rules={[{ required: true, message: 'Please input your username!' }]}
     >
       <Input />
     </Form.Item>
 
-    <Form.Item
+    <Form.Item<FieldType>
       label="Password"
       name="password"
       rules={[{ required: true, message: 'Please input your password!' }]}
@@ -47,30 +42,19 @@ const LoginForm: React.FC = () => (
       <Input.Password />
     </Form.Item>
 
-    <Form.Item style={{ marginLeft: "21.5vh" }}>
-      <Form.Item name="remember" valuePropName="checked" noStyle>
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
-      <a className="login-form-forgot" href="" style={{ marginLeft: "1vh" }}>
-        Forgot password
-      </a>
+    <Form.Item<FieldType>
+      name="remember"
+      valuePropName="checked"
+      wrapperCol={{ offset: 8, span: 16 }}
+    >
+      <Checkbox>Remember me</Checkbox>
     </Form.Item>
 
     <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
       <Button type="primary" htmlType="submit">
-        Sign In
+        Submit
       </Button>
-      <span style={{ marginLeft: "0.5vh" }}>Or <Link to="/register">register now!</Link></span>
     </Form.Item>
-
-    <Divider style={{ paddingLeft: "20vh" }}>Or... test the demo version</Divider>
-    <Link to="/App">
-      <Avatar
-        size={{ xs: 24, sm: 32, md: 40, lg: 64, xl: 80, xxl: 100 }}
-        style={{ marginLeft: 350 }}
-        icon={<UserOutlined />}
-      />
-    </Link>
   </Form>
 );
 
